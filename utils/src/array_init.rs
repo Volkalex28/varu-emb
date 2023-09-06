99185c833bc80c6d2fb295407a019f884fba4e02
Bohdan Shubenok <bohdan.shubenok@sigma.software>
2023-09-06 09:09:08 +0300

    Bring back `with_data` function to ArrayInitializer

--- utils/src/array_init.rs
+++ utils/src/array_init.rs
@@ -1,6 +1,9 @@
 use core::mem::MaybeUninit as UnInit;
+use std::marker::Destruct;
 
 pub struct ArrayInitializer {}
+
+#[allow(unused)]
 impl ArrayInitializer {
     pub const fn init<const S: usize, T, F: ~const Fn(usize) -> T>(init_func: &F) -> [T; S] {
         let mut i = 0;
@@ -10,6 +13,30 @@ impl ArrayInitializer {
                 break ret;
             }
             ret[i].write(init_func(i));
+            i += 1;
+        };
+        unsafe { ret_data.transpose().assume_init() }
+    }
+
+    const fn with_data<T, D, const S: usize, F>(init_func: &F, data: D) -> [T; S]
+    where
+        T: ~const Destruct,
+        D: ~const Destruct,
+        F: ~const Fn(usize, D) -> (T, D),
+    {
+        let mut i = 0;
+        let mut data = data;
+
+        let mut ret = UnInit::<[T; S]>::uninit().transpose();
+        let ret_data = loop {
+            if i >= S {
+                break ret;
+            }
+
+            let (new_value, new_data) = init_func(i, data);
+
+            ret[i].write(new_value);
+            data = new_data;
             i += 1;
         };
         unsafe { ret_data.transpose().assume_init() }
